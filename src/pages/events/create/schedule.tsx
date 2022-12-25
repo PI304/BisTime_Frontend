5894a6fd7ca7083e93ee043b74f4022f7d4271c6
jooooonis <xowns3213@naver.com>
2022-12-25 22:57:08 +0900

    feat : schedule page use time-picker

--- src/pages/events/create/schedule.tsx
+++ src/pages/events/create/schedule.tsx
@@ -1,15 +1,25 @@
 import { useRouter } from 'next/router';
 import Layout from '@components/common/layout';
 import Calender from '@components/common/calender';
-import Select from '@components/common/select';
-import Toggle from '@components/common/toggle';
 import Button from '@components/common/button';
+import { useState } from 'react';
+import TimePicker from '@components/common/time-picker';
+import { useForm } from 'react-hook-form';
 
 function Schedule() {
+  const { setValue, handleSubmit } = useForm();
+
+  const onValid = (form) => {
+    console.log(form);
+    router.push('/events/create/summary');
+  };
   const router = useRouter();
   return (
     <Layout>
-      <div className="w-full flex flex-col items-center justify-center h-full">
+      <form
+        onSubmit={handleSubmit(onValid)}
+        className="w-full flex flex-col items-center justify-center h-full"
+      >
         <div className="w-full flex flex-col items-center justify-center mb-16">
           <h1 className="text-h2 font-bold text-center text-base-black">
             Choose WeekDays
@@ -19,25 +29,17 @@ function Schedule() {
           <Calender />
         </div>
         <div className="w-full flex items-center justify-between mt-4">
-          <div className="flex justify-center items-center">
-            <Select />
-            <Toggle className="ml-1" />
-          </div>
-          <div className="flex justify-center items-center">
-            <Select />
-            <Toggle className="ml-1" />
-          </div>
+          <TimePicker
+            name="start_time"
+            dayOrNight={false}
+            setValue={setValue}
+          />
+          <TimePicker name="end_time" dayOrNight={true} setValue={setValue} />
         </div>
         <div className="w-full flex items-center justify-center mt-4">
-          <Button
-            onClick={() => {
-              router.push('/events/create/summary');
-            }}
-          >
-            Next
-          </Button>
+          <Button>Next</Button>
         </div>
-      </div>
+      </form>
     </Layout>
   );
 }
