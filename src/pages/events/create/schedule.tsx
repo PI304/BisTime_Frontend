311defaaa9629bfbd6c7f1ce224b36147c0d84c0
jooooonis <xowns3213@naver.com>
2022-12-29 20:07:39 +0900

    feat : setTime global

--- src/pages/events/create/schedule.tsx
+++ src/pages/events/create/schedule.tsx
@@ -2,18 +2,33 @@ import { useRouter } from 'next/router';
 import Layout from '@components/common/layout';
 import Calender from '@components/common/calender';
 import Button from '@components/common/button';
-import { useState } from 'react';
 import TimePicker from '@components/common/time-picker';
 import { useForm } from 'react-hook-form';
+import { useAppDispatch, useAppSelector } from '@features/hooks';
+import { setTime } from '@features/event/eventSlice';
+
+type time = {
+  value: string;
+};
+interface ScheuleForm {
+  start_time: time;
+  end_time: time;
+}
 
 function Schedule() {
-  const { setValue, handleSubmit } = useForm();
+  const { setValue, handleSubmit } = useForm<ScheuleForm>();
+  const dispatch = useAppDispatch();
+  const eventState = useAppSelector((state) => state.event);
+  const router = useRouter();
 
-  const onValid = (form) => {
-    console.log(form);
+  const onValid = (form: ScheuleForm) => {
+    const { start_time, end_time } = form;
+    dispatch(
+      setTime({ start_time: start_time.value, end_time: end_time.value }),
+    );
     router.push('/events/create/summary');
   };
-  const router = useRouter();
+
   return (
     <Layout>
       <form
