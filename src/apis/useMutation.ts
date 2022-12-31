3ff023e8d6b8178c8e246ff7b35445260b75f06b
jooooonis <xowns3213@naver.com>
2022-12-31 16:15:26 +0900

    feat : useMuation hook

---
+++ src/apis/useMutation.ts
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+
+interface useMutationState {
+  loading: boolean;
+  data?: object;
+  error?: object;
+}
+
+type UseMutationResult = [(data: any) => void, useMutationState];
+
+export default function useMutation(url: string): UseMutationResult {
+  const [state, setState] = useState<useMutationState>({
+    loading: false,
+    data: undefined,
+    error: undefined,
+  });
+
+  function mutation(data: any) {
+    setState((prev) => ({ ...prev, loading: true }));
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .catch(() => {
+        //json이 없는경우 예외처리 일단 그냥 둠
+      })
+      .then((data) => setState((prev) => ({ ...prev, data })))
+      .catch((error) => setState((prev) => ({ ...prev, error })))
+      .finally(() => setState((prev) => ({ ...prev, loading: false })));
+  }
+
+  return [mutation, { ...state }];
+}
