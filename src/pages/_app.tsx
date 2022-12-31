0737d12856533ac170dc71746e41b8878cbdf765
jooooonis <xowns3213@naver.com>
2022-12-31 16:35:43 +0900

    feat : swr config

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,17 +4,24 @@ import { Provider } from 'react-redux';
 import store from '../features/store';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistStore } from 'redux-persist';
+import { SWRConfig } from 'swr';
 
 const persistor = persistStore(store);
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className="mx-auto w-full max-w-[375px]">
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <Component {...pageProps} />
-        </PersistGate>
-      </Provider>
-    </div>
+    <SWRConfig
+      value={{
+        fetcher: (url: string) => fetch(url).then((res) => res.json()),
+      }}
+    >
+      <div className="mx-auto w-full max-w-[375px]">
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <Component {...pageProps} />
+          </PersistGate>
+        </Provider>
+      </div>
+    </SWRConfig>
   );
 }
