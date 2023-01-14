ace37447629fd31b45946d91e1d4341a19582cc1
jooooonis <xowns3213@naver.com>
2023-01-15 02:20:59 +0900

    design : fontWeight

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,16 @@
 import { useRouter } from 'next/router';
 import Layout from '@components/common/layout';
 import Button from '@components/common/button';
+import Navigate from '@components/common/navigate';
 
 function Home() {
   const router = useRouter();
   return (
     <Layout>
+      <Navigate />
       <div className="w-full flex flex-col items-center justify-center h-full">
         <div className="w-full flex flex-col items-center justify-center mb-16">
-          <h1 className="text-h2 font-bold text-center text-base-black">
+          <h1 className="text-h2 font-normal text-center text-base-black">
             Create Your Own Plan <br />
             And Share with teams
           </h1>
