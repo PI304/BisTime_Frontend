e0277d0ee88075989108aa98e00c959b196416ea
jooooonis <xowns3213@naver.com>
2023-01-15 02:17:49 +0900

    feat : default navigate

---
+++ src/components/common/navigate.tsx
@@ -0,0 +1,21 @@
+export default function Navigate() {
+  return (
+    <div className="fixed top-0 w-full bg-white flex h-[60px] items-center">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="2"
+        stroke="currentColor"
+        className="w-6 h-6"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+        />
+      </svg>
+      <div className="text-[20px] ml-2 font-bold">BISTIMES</div>
+    </div>
+  );
+}
