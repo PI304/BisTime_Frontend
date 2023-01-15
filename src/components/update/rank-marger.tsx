9a488e0f361ce3184c62e327eec8cf09ee1bed43
jooooonis <xowns3213@naver.com>
2023-01-15 17:46:18 +0900

    feat : rank-marker component

---
+++ src/components/update/rank-marger.tsx
@@ -0,0 +1,33 @@
+export default function RankMarker() {
+  return (
+    <div className="space-y-1">
+      <div className="flex space-x-3 items-center">
+        <span className="text-[12px] font-bold w-10 flex justify-center items-center align-baseline h-6 rounded bg-primary-green-2 text-primary-green-1">
+          1st
+        </span>
+        <span className="text-[14px] font-bold text-primary-green-1">
+          16:00
+        </span>
+        <span className="text-normal text-[12px]">7 people</span>
+      </div>
+      <div className="flex space-x-3 items-center">
+        <span className="text-[12px] font-bold w-10 flex justify-center items-center align-baseline h-6 rounded bg-primary-green-2 text-primary-green-1">
+          2nd
+        </span>
+        <span className="text-[14px] font-bold text-primary-green-1">
+          16:00
+        </span>
+        <span className="text-normal text-[12px]">7 people</span>
+      </div>
+      <div className="flex space-x-3 items-center">
+        <span className="text-[12px] font-bold w-10 flex justify-center items-center align-baseline h-6 rounded bg-primary-green-2 text-primary-green-1">
+          3rd
+        </span>
+        <span className="text-[14px] font-bold text-primary-green-1">
+          16:00
+        </span>
+        <span className="text-normal text-[12px]">7 people</span>
+      </div>
+    </div>
+  );
+}
