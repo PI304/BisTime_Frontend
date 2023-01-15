0efb760ad5c4a369b27b10ab4c102e4b24859227
jooooonis <xowns3213@naver.com>
2023-01-15 16:54:59 +0900

    feat : date component

---
+++ src/components/update/date.tsx
@@ -0,0 +1,12 @@
+export default function Date() {
+  return (
+    <div className="flex items-center">
+      <div className="text-[60px] font-normal">16</div>
+      <div className="ml-3 text-[16px] text-gray-6 font-normal leading-5">
+        Wed <br />
+        Nov <br />
+        2022
+      </div>
+    </div>
+  );
+}
