3954f506abdf1a621cef2a20bdf634d665a7b73a
jooooonis <xowns3213@naver.com>
2023-01-15 16:11:40 +0900

    fix : camelcase

--- src/components/common/navigate.tsx
+++ src/components/common/navigate.tsx
@@ -27,13 +27,13 @@ export default function Navigate({
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="2"
+          strokeWidth="2"
           stroke="currentColor"
           className="w-6 h-6 cursor-pointer"
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
           />
         </svg>
@@ -43,7 +43,7 @@ export default function Navigate({
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke-width="2"
+          strokeWidth="2"
           stroke="currentColor"
           className="w-6 h-6 cursor-pointer"
           onClick={() => {
@@ -51,8 +51,8 @@ export default function Navigate({
           }}
         >
           <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
             d="M15.75 19.5L8.25 12l7.5-7.5"
           />
         </svg>
