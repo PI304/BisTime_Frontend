ff38c7abd1d201bd09c28a6824184a5cd334917c
jooooonis <xowns3213@naver.com>
2023-02-16 15:46:45 +0900

    feat : fontweight config

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,11 @@ module.exports = {
     './src/components/**/*.{js,jsx,ts,tsx}',
   ],
   theme: {
+    fontWeight: {
+      light: 300,
+      medium: 500,
+      bold: 700,
+    },
     extend: {
       fontFamily: {
         Gmarket: ['GmarketSans', 'sans-serif'],
