7d10ca64cf1e9170c718e563f2244da45689ee44
jooooonis <xowns3213@naver.com>
2023-02-16 15:48:19 +0900

    feat : index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,21 +7,20 @@ function Home() {
   const router = useRouter();
   return (
     <Layout>
-      <Navigate />
+      <Navigate back />
       <div className="w-full flex flex-col items-center justify-center h-full">
-        <div className="w-full flex flex-col items-center justify-center mb-16">
-          <h1 className="text-h2 font-normal text-center text-base-black">
-            Create Your Own Plan <br />
-            And Share with teams
-          </h1>
+        <div className="w-full flex flex-col items-center justify-center mb-4">
+          <div className="text-14 text-left w-full">
+            <span className="font-bold">BISTIME</span>은 시간 관리를 위한 간단한
+            도구 입니다.
+          </div>
+          <div className="text-14 w-full mt-2">
+            모임을 만들고, 모임에 참여하고, 모임의 일정을 관리해보세요.
+          </div>
         </div>
-        <div className="w-full space-y-4 flex flex-col items-center justify-center">
-          <Button size="lg" onClick={() => router.push('/events/create')}>
-            Events
-          </Button>
-          <h1 className="text-b2 font-bold text-center text-base-black">OR</h1>
-          <Button size="lg" onClick={() => router.push('/events/create')}>
-            Teams
+        <div className="w-full space-y-2 flex flex-col items-center justify-center">
+          <Button onClick={() => router.push('/events/create')}>
+            모임 만들러 가기
           </Button>
         </div>
       </div>
