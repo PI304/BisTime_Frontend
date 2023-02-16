767fee1e46dab06c292c52964ade3289b7cf1916
jooooonis <xowns3213@naver.com>
2023-02-16 16:53:53 +0900

    feat : error-message component

---
+++ src/components/common/error-message.tsx
@@ -0,0 +1,14 @@
+import { ErrorSVG } from 'styles/svgs';
+
+interface ErrorMessageProps {
+  message: string;
+}
+
+export default function ErrorMessage({ message }: ErrorMessageProps) {
+  return (
+    <p className="w-full mt-2 text-left flex text-system-error text-12 font-light">
+      <span className="mr-0.5">{ErrorSVG}</span>
+      {message}
+    </p>
+  );
+}
