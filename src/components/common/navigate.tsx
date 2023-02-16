ceebe0f3ee9ad5063e4f56a906db0c46d10a3bb1
jooooonis <xowns3213@naver.com>
2023-02-16 15:20:01 +0900

    feat : navigate component

--- src/components/common/navigate.tsx
+++ src/components/common/navigate.tsx
@@ -1,83 +1,31 @@
 import { useRouter } from 'next/router';
+import { ArrowLeftSVG } from 'styles/svgs';
 
 interface NavigateProps {
-  left?: 'menu' | 'back' | 'none';
-  center?: 'logo' | 'title' | 'none';
-  right?: 'user' | 'none';
-  title?: string;
-  user?: string;
+  back?: boolean;
+  mode?: 'event' | 'team';
   className?: string;
 }
 
 export default function Navigate({
-  left = 'menu',
-  center = 'logo',
-  right = 'none',
-  title,
-  user,
+  back = false,
+  mode = 'event',
   className,
 }: NavigateProps) {
   const router = useRouter();
   return (
     <div
-      className={`fixed top-0 max-w-[335px] w-full bg-white flex h-[60px] items-center ${
-        center === 'title' ? 'justify-between' : ''
-      } ${className}`}
+      className={`fixed top-0 max-w-[335px] w-full bg-white flex h-16 items-center ${className}`}
     >
-      {left === 'menu' && (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="2"
-          stroke="currentColor"
-          className="w-6 h-6 cursor-pointer"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-          />
-        </svg>
-      )}
-      {left === 'back' && (
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="2"
-          stroke="currentColor"
-          className="w-6 h-6 cursor-pointer"
-          onClick={() => {
-            router.back();
-          }}
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M15.75 19.5L8.25 12l7.5-7.5"
-          />
-        </svg>
-      )}
-      {left === 'none' && <div className="w-6 h-6" />}
-      {center === 'logo' && (
-        <div
-          onClick={() => {
-            router.push('/');
-          }}
-          className="text-[20px] ml-2 font-bold cursor-pointer"
-        >
-          BISTIME
-        </div>
-      )}
-      {center === 'title' && (
-        <div className="text-[20px] font-medium">{title}</div>
-      )}
-      {center === 'none' && <div className="w-6 h-6" />}
-      {right === 'user' && (
-        <div className="text-[16px] font-medium">{user}</div>
-      )}
-      {right === 'none' && <div className="w-6 h-6" />}
+      {back && ArrowLeftSVG}
+      <div
+        onClick={() => {
+          router.push('/');
+        }}
+        className="text-18 font-bold cursor-pointer"
+      >
+        BISTIME<span className="text-12 ml-0.5 font-light">{mode}</span>
+      </div>
     </div>
   );
 }
