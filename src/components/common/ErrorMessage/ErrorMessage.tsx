4d116fe4cd61c3a8ab7d86e591d9745642a7321b
jooooonis <xowns3213@naver.com>
2023-02-19 00:46:37 +0900

    fix : 날짜 잘못 선택해도 페이지 넘어가는 오류 해결

--- src/components/common/ErrorMessage/ErrorMessage.tsx
+++ src/components/common/ErrorMessage/ErrorMessage.tsx
@@ -2,11 +2,17 @@ import Image from 'next/image';
 
 interface ErrorMessageProps {
   message: string;
+  className?: string;
 }
 
-export default function ErrorMessage({ message }: ErrorMessageProps) {
+export default function ErrorMessage({
+  message,
+  className,
+}: ErrorMessageProps) {
   return (
-    <p className="w-full mt-2 text-left flex text-system-error text-12 font-light">
+    <p
+      className={`w-full mt-2 text-left flex text-system-error text-12 font-light ${className}`}
+    >
       <span className="mr-0.5">
         <Image
           src="/svg/icons/Info_circle.svg"
--- src/pages/event/create/create02.tsx
+++ src/pages/event/create/create02.tsx
@@ -12,7 +12,6 @@ import { useEffect } from 'react';
 
 function Create() {
   const {
-    setValue,
     handleSubmit,
     setError,
     formState: { errors },
@@ -21,22 +20,26 @@ function Create() {
 
   const router = useRouter();
   const eventState = useAppSelector((state) => state.event);
+  console.log(eventState.additional_dates);
   const onSubmit = () => {
     const today = moment();
+
+    let isDateValid = true;
+
     eventState.additional_dates.map((date) => {
       const someDate = moment(date);
-      console.log(someDate.diff(today, 'days'));
       if (someDate.diff(today) <= 0) {
-        console.log('오늘 이전의 날짜는 선택할 수 없습니다.');
-        setError('date', {
-          type: 'manual',
-          message: '오늘 이전의 날짜는 선택할 수 없습니다.',
-        });
-        return;
+        isDateValid = false;
       }
     });
 
-    router.push('/event/create/create03');
+    if (!isDateValid) {
+      setError('date', {
+        type: 'manual',
+        message: '오늘 이전의 날짜는 선택할 수 없습니다.',
+      });
+      return;
+    } else router.push('/event/create/create03');
   };
 
   useEffect(() => {
@@ -58,7 +61,10 @@ function Create() {
             여러 날의 선택이 가능합니다.
           </div>
           {errors.date && (
-            <ErrorMessage message={errors.date.message as string} />
+            <ErrorMessage
+              className="mt-0"
+              message={errors.date.message as string}
+            />
           )}
         </div>
         <div className="w-full flex items-center justify-center">
