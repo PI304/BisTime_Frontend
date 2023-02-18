67d4a0bace73d3dcfb60b8edc13e502f94693ba0
jooooonis <xowns3213@naver.com>
2023-02-19 01:44:10 +0900

    feat : 전역 type 사용

--- src/apis/event/eventApi.query.ts
+++ src/apis/event/eventApi.query.ts
@@ -1,7 +1,6 @@
 import { AxiosError } from 'axios';
 import { useQuery } from 'react-query';
 import eventApi from './eventApi';
-import { EventDate } from './eventApi.type';
 
 export const useGetEventDateQuery = (uuid: string) => {
   return useQuery<EventDate, AxiosError>(['event', uuid], () =>
