ce1ed90fcca5f8b0557db7864cfb4afd72e79e1c
jooooonis <xowns3213@naver.com>
2023-02-18 16:27:05 +0900

    chore : icon svg 사용

--- src/components/common/ErrorMessage/ErrorMessage.tsx
+++ src/components/common/ErrorMessage/ErrorMessage.tsx
@@ -1,4 +1,4 @@
-import { ErrorSVG } from '@public/svg';
+import Image from 'next/image';
 
 interface ErrorMessageProps {
   message: string;
@@ -7,7 +7,14 @@ interface ErrorMessageProps {
 export default function ErrorMessage({ message }: ErrorMessageProps) {
   return (
     <p className="w-full mt-2 text-left flex text-system-error text-12 font-light">
-      <span className="mr-0.5">{ErrorSVG}</span>
+      <span className="mr-0.5">
+        <Image
+          src="/svg/icons/Info_circle.svg"
+          width={14}
+          height={14}
+          alt="info"
+        />
+      </span>
       {message}
     </p>
   );
