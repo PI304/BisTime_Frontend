0776d81663e8e77604cf48354a3dfc68215e27fc
jooooonis <xowns3213@naver.com>
2023-02-22 15:38:05 +0900

    feat : DashBoard 컴포넌트화

--- src/pages/event/index.tsx
+++ src/pages/event/index.tsx
@@ -1,11 +1,12 @@
-import Navigate from '@components/common/Navigate';
 import Layout from '@components/common/Layout';
+import Navigate from '@components/common/Navigate';
+import DashBoard from '@components/event/Calender/DashBoard';
 
 export default function Event() {
   return (
     <Layout>
-      <Navigate back link />
-      <div className="w-full flex flex-col pt-2">
+      <Navigate link />
+      <div className="w-full flex flex-col mt-4">
         <div className="w-full flex items-center justify-between">
           <div className="flex items-center">
             <div className="text-24">주니 생일</div>
@@ -20,65 +21,8 @@ export default function Event() {
             <div className="w-1 aspect-square rounded-full bg-gray-5" />
           </div>
         </div>
-        <div className="mt-4">
-          <div className="text-12">2023년 1월 14일 (토)</div>
-          <div className="grid grid-cols-6 grid-rows-1">
-            {[1, 2, 3, 4, 5, 6].map((_, index) => (
-              <div key={index}>
-                <p className="text-left font-light text-12 mt-2">18</p>
-                <div
-                  className={`w-full aspect-square mt-1 bg-primary-green-1 bg-opa bg-opacity-${
-                    Math.floor(Math.random() * 20) * 5 + 5
-                  }`}
-                />
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="mt-4">
-          <div className="text-12">2023년 1월 14일 (토)</div>
-          <div className="grid grid-cols-6 grid-rows-1">
-            {[1, 2, 3, 4, 5, 6].map((_, index) => (
-              <div key={index}>
-                <p className="text-left font-light text-12 mt-2">18</p>
-                <div
-                  className={`w-full aspect-square mt-1 bg-primary-green-1 bg-opa bg-opacity-${
-                    Math.floor(Math.random() * 20) * 5 + 5
-                  }`}
-                />
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="mt-4">
-          <div className="text-12">2023년 1월 14일 (토)</div>
-          <div className="grid grid-cols-6 grid-rows-1">
-            {[1, 2, 3, 4, 5, 6].map((_, index) => (
-              <div key={index}>
-                <p className="text-left font-light text-12 mt-2">18</p>
-                <div
-                  className={`w-full aspect-square mt-1 bg-primary-green-1 bg-opa bg-opacity-${
-                    Math.floor(Math.random() * 20) * 5 + 5
-                  }`}
-                />
-              </div>
-            ))}
-          </div>
-        </div>
-        <div className="mt-4">
-          <div className="text-12">2023년 1월 14일 (토)</div>
-          <div className="grid grid-cols-6 grid-rows-1">
-            {[1, 2, 3, 4, 5, 6].map((_, index) => (
-              <div key={index}>
-                <p className="text-left font-light text-12 mt-2">18</p>
-                <div
-                  className={`w-full aspect-square mt-1 bg-primary-green-1 bg-opa bg-opacity-${
-                    Math.floor(Math.random() * 20) * 5 + 5
-                  }`}
-                />
-              </div>
-            ))}
-          </div>
+        <div>
+          <DashBoard />
         </div>
       </div>
     </Layout>
