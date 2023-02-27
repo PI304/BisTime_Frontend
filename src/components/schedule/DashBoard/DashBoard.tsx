232e2705c89b99f68abe492b4cfaf3980f5fdec7
jooooonis <xowns3213@naver.com>
2023-02-27 15:38:17 +0900

    feat : 일정 등록 대시보드 컴포넌트

---
+++ src/components/schedule/DashBoard/DashBoard.tsx
@@ -0,0 +1,179 @@
+import Image from 'next/image';
+
+interface DashBoardProps {
+  date: string;
+  members: string[];
+  startIdx: number;
+  endIdx: number;
+  availability: string;
+}
+
+const TIMETABLE = [
+  '00:00',
+  '00:30',
+  '01:00',
+  '01:30',
+  '02:00',
+  '02:30',
+  '03:00',
+  '03:30',
+  '04:00',
+  '04:30',
+  '05:00',
+  '05:30',
+  '06:00',
+  '06:30',
+  '07:00',
+  '07:30',
+  '08:00',
+  '08:30',
+  '09:00',
+  '09:30',
+  '10:00',
+  '10:30',
+  '11:00',
+  '11:30',
+  '12:00',
+  '12:30',
+  '13:00',
+  '13:30',
+  '14:00',
+  '14:30',
+  '15:00',
+  '15:30',
+  '16:00',
+  '16:30',
+  '17:00',
+  '17:30',
+  '18:00',
+  '18:30',
+  '19:00',
+  '19:30',
+  '20:00',
+  '20:30',
+  '21:00',
+  '21:30',
+  '22:00',
+  '22:30',
+  '23:00',
+  '23:30',
+];
+
+function getOpacity(n, x) {
+  if (x === 0) {
+    return 10;
+  }
+  const result = Math.round((x / n) * 10) * 10;
+  return Math.max(10, Math.min(100, result));
+}
+
+export default function DashBoard({
+  date,
+  members,
+  startIdx,
+  endIdx,
+  availability,
+}: DashBoardProps) {
+  return (
+    <div className="mt-4">
+      <div className="text-12">{date}</div>
+      <div className="grid mt-1 grid-cols-6 gap-y-1 grid-rows-1">
+        {members &&
+          availability
+            ?.slice(startIdx, endIdx)
+            .split('')
+            .map((availalbe, index) => {
+              const time =
+                (startIdx + index) % 2 === 0 ? TIMETABLE[startIdx + index] : '';
+              return (
+                <div key={index}>
+                  {time !== '' ? (
+                    <p className={`text-left font-light text-10 h-4 relative`}>
+                      {time}
+                    </p>
+                  ) : (
+                    <Image
+                      src="/svg/icons/triangle.svg"
+                      width={8}
+                      height={8}
+                      alt="triangle"
+                      className="h-4 -translate-x-1 "
+                    />
+                  )}
+                  {getOpacity(members.length, parseInt(availalbe)) <= 10 && (
+                    <div className="w-full cursor-pointer aspect-square bg-primary-green-1 bg-opacity-10">
+                      <p className="w-full text-12 text-primary-green-3 aspect-square flex justify-center items-center opacity-0 transition hover:opacity-100">
+                        {availalbe + '/' + members.length}
+                      </p>
+                    </div>
+                  )}
+                  {getOpacity(members.length, parseInt(availalbe)) === 20 && (
+                    <div className="w-full cursor-pointer aspect-square bg-primary-green-1 bg-opacity-20">
+                      <p className="w-full text-12 text-primary-green-3 aspect-square flex justify-center items-center opacity-0 transition hover:opacity-100">
+                        {availalbe + '/' + members.length}
+                      </p>
+                    </div>
+                  )}
+                  {getOpacity(members.length, parseInt(availalbe)) === 30 && (
+                    <div className="w-full cursor-pointer aspect-square bg-primary-green-1 bg-opacity-30">
+                      <p className="w-full text-12 text-primary-green-3 aspect-square flex justify-center items-center opacity-0 transition hover:opacity-100">
+                        {availalbe + '/' + members.length}
+                      </p>
+                    </div>
+                  )}{' '}
+                  {getOpacity(members.length, parseInt(availalbe)) === 40 && (
+                    <div className="w-full cursor-pointer aspect-square bg-primary-green-1 bg-opacity-40">
+                      <p className="w-full text-12 text-primary-green-3 aspect-square flex justify-center items-center opacity-0 transition hover:opacity-100">
+                        {availalbe + '/' + members.length}
+                      </p>
+                    </div>
+                  )}{' '}
+                  {getOpacity(members.length, parseInt(availalbe)) === 50 && (
+                    <div className="w-full cursor-pointer aspect-square bg-primary-green-1 bg-opacity-50">
+                      <p className="w-full text-12 text-primary-green-3 aspect-square flex justify-center items-center opacity-0 transition hover:opacity-100">
+                        {availalbe + '/' + members.length}
+                      </p>
+                    </div>
+                  )}{' '}
+                  {getOpacity(members.length, parseInt(availalbe)) === 60 && (
+                    <div className="w-full cursor-pointer aspect-square bg-primary-green-1 bg-opacity-60">
+                      <p className="w-full text-12 text-primary-green-3 aspect-square flex justify-center items-center opacity-0 transition hover:opacity-100">
+                        {availalbe + '/' + members.length}
+                      </p>
+                    </div>
+                  )}{' '}
+                  {getOpacity(members.length, parseInt(availalbe)) === 70 && (
+                    <div className="w-full cursor-pointer aspect-square bg-primary-green-1 bg-opacity-70">
+                      <p className="w-full text-12 text-primary-green-3 aspect-square flex justify-center items-center opacity-0 transition hover:opacity-100">
+                        {availalbe + '/' + members.length}
+                      </p>
+                    </div>
+                  )}{' '}
+                  {getOpacity(members.length, parseInt(availalbe)) === 80 && (
+                    <div className="w-full cursor-pointer aspect-square bg-primary-green-1 bg-opacity-80">
+                      <p className="w-full text-12 text-primary-green-3 aspect-square flex justify-center items-center opacity-0 transition hover:opacity-100">
+                        {availalbe + '/' + members.length}
+                      </p>
+                    </div>
+                  )}{' '}
+                  {getOpacity(members.length, parseInt(availalbe)) === 90 && (
+                    <div className="w-full cursor-pointer aspect-square bg-primary-green-1 bg-opacity-90">
+                      <p className="w-full text-12 text-primary-green-3 aspect-square flex justify-center items-center opacity-0 transition hover:opacity-100">
+                        {availalbe + '/' + members.length}
+                      </p>
+                    </div>
+                  )}{' '}
+                  {getOpacity(members.length, parseInt(availalbe)) === 100 && (
+                    <div className="w-full cursor-pointer aspect-square bg-primary-green-1 bg-opacity-100">
+                      <p className="w-full text-12 text-primary-green-3 aspect-square flex justify-center items-center opacity-0 transition hover:opacity-100">
+                        {availalbe + '/' + members.length}
+                      </p>
+                    </div>
+                  )}
+                </div>
+              );
+            })}
+      </div>
+    </div>
+  );
+}
--- src/pages/event/schedule/add03.tsx
+++ src/pages/event/schedule/add03.tsx
@@ -1,90 +1,112 @@
 import Button from '@components/common/Button';
-import ErrorMessage from '@components/common/ErrorMessage';
 import Layout from '@components/common/Layout';
 import Navigate from '@components/common/Navigate';
 import ProgressBar from '@components/common/ProgressBar';
-import { useForm } from 'react-hook-form';
-import { useAppSelector } from '@features/hooks';
-import { useEffect } from 'react';
-import { TimePicker } from '@components/common/TimePicker';
 import { usePostEventMutation } from '@apis/event/eventApi.mutation';
+import { useGetEventQuery } from '@apis/event/eventApi.query';
+import { useGetScheduleQuery } from '@apis/schedule/scheduleApi.query';
+import { useRouter } from 'next/router';
+import { formatDateWithDayOfWeek } from '@utils/formatDate';
+import DashBoard from '@components/schedule/DashBoard';
 
-type time = {
-  value: string;
+const scheduleListToMembers = (scheduleList: Schedule[]) => {
+  const members = scheduleList.map((item) => item.name);
+  const memberSet = new Set(members);
+  const memberArray = Array.from(memberSet);
+  return memberArray;
 };
-
-interface ScheuleForm {
-  startTime: time;
-  endTime: time;
-}
-
+const TIMETABLE = [
+  '00:00',
+  '00:30',
+  '01:00',
+  '01:30',
+  '02:00',
+  '02:30',
+  '03:00',
+  '03:30',
+  '04:00',
+  '04:30',
+  '05:00',
+  '05:30',
+  '06:00',
+  '06:30',
+  '07:00',
+  '07:30',
+  '08:00',
+  '08:30',
+  '09:00',
+  '09:30',
+  '10:00',
+  '10:30',
+  '11:00',
+  '11:30',
+  '12:00',
+  '12:30',
+  '13:00',
+  '13:30',
+  '14:00',
+  '14:30',
+  '15:00',
+  '15:30',
+  '16:00',
+  '16:30',
+  '17:00',
+  '17:30',
+  '18:00',
+  '18:30',
+  '19:00',
+  '19:30',
+  '20:00',
+  '20:30',
+  '21:00',
+  '21:30',
+  '22:00',
+  '22:30',
+  '23:00',
+  '23:30',
+];
 function Create() {
-  const {
-    setValue,
-    handleSubmit,
-    setError,
-    watch,
-    clearErrors,
-    formState: { errors },
-  } = useForm<ScheuleForm>();
-  const eventState = useAppSelector((state) => state.event);
+  const router = useRouter();
+  const { uuid } = router.query;
+  const { data: event, isLoading } = useGetEventQuery(uuid as string);
+  const { data: scheduleList } = useGetScheduleQuery(uuid as string);
+  const members = scheduleListToMembers(scheduleList || []);
+  const startIndex = TIMETABLE.indexOf(event?.startTime || '00:00');
+  const endIndex = TIMETABLE.indexOf(event?.endTime || '00:00');
 
-  const { mutate, isLoading } = usePostEventMutation();
-  const onSubmit = (form: ScheuleForm) => {
-    const { startTime, endTime } = form;
-    if (startTime.value > endTime.value) {
-      setError('startTime', {
-        type: 'manual',
-        message: '시작시간이 종료시간보다 늦을 수 없습니다.',
-      });
-      return;
-    }
-    mutate({
-      title: eventState.title,
-      startTime: startTime.value,
-      endTime: endTime.value,
-    });
-  };
-  useEffect(
-    () => clearErrors(),
-    [clearErrors, watch('startTime'), watch('endTime')],
-  );
+  const { mutate } = usePostEventMutation();
 
   return (
-    <Layout>
+    <Layout className="relative">
       <Navigate back />
       <ProgressBar progress="w-full" className="mt-3" />
-      <form
-        onSubmit={handleSubmit(onSubmit)}
-        className="mt-6 w-full space-y-5 flex flex-col items-center justify-center"
-      >
+      <form className="mt-6 w-full flex flex-col items-center justify-center">
         <div className="w-full flex flex-col items-center justify-center">
           <div className="text-18 text-left w-full">
             모임 시간을 설정하세요.
           </div>
-          <div className="text-18 text-left w-full">30분 단위로</div>
-          <div className="text-18 text-left w-full">가능여부를 조사합니다.</div>
-          {errors.startTime && (
-            <ErrorMessage message={errors.startTime.message} />
-          )}
+          <div className="text-18 text-left w-full">30분 단위로 가능한</div>
+          <div className="text-18 text-left w-full">시간대에 체크하세요.</div>
         </div>
-
-        <div className="w-full flex items-center justify-between mt-4">
-          <TimePicker
-            name="startTime"
-            label="시작시간"
-            dayOrNight={false}
-            setValue={setValue}
-          />
-          <TimePicker
-            name="endTime"
-            label="종료시간"
-            dayOrNight={true}
-            setValue={setValue}
-          />
+        <div className="w-full">
+          {Object.keys(event?.availability || {}).map((date) => (
+            <DashBoard
+              key={date}
+              members={members}
+              date={formatDateWithDayOfWeek(date)}
+              startIdx={startIndex}
+              endIdx={endIndex}
+              availability={event?.availability[date]}
+            />
+          ))}
         </div>
         <div className="w-full flex items-center justify-center mt-5">
-          <Button loading={isLoading}>이벤트 만들기</Button>
+          <Button
+            loading={isLoading}
+            className="absolute bottom-4 w-[calc(100%-40px)]"
+          >
+            일정 등록 하기
+          </Button>
         </div>
       </form>
     </Layout>
