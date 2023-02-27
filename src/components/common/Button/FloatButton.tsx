08b31d9a0f2e6344e3cadf5bc4842a0e987d7c92
jooooonis <xowns3213@naver.com>
2023-02-27 14:37:43 +0900

    feat : FloatButton 컴포넌트 작성

---
+++ src/components/common/Button/FloatButton.tsx
@@ -0,0 +1,47 @@
+import Image from 'next/image';
+
+interface FloatButtonProps {
+  type?: 'add' | 'filter';
+  onClick?: () => void;
+  className?: string;
+}
+
+export default function FloatButton({
+  type = 'add',
+  onClick,
+  className,
+  ...rest
+}: FloatButtonProps) {
+  if (type === 'add')
+    return (
+      <button
+        className={`absolute right-5 bottom-10 w-12 aspect-square rounded-full bg-primary-green-1 ${className}`}
+        onClick={onClick}
+        {...rest}
+      >
+        <Image
+          src="/svg/icons/plus.svg"
+          width={28}
+          height={28}
+          alt="filter"
+          className="m-auto"
+        />
+      </button>
+    );
+  if (type === 'filter')
+    return (
+      <button
+        className={`absolute right-5 bottom-24 w-12 aspect-square rounded-full bg-primary-green-1 ${className}`}
+        onClick={onClick}
+        {...rest}
+      >
+        <Image
+          src="/svg/icons/filter.svg"
+          width={24}
+          height={24}
+          alt="filter"
+          className="m-auto"
+        />
+      </button>
+    );
+}
