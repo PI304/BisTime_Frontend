9bcd609bac5a841a8ece9886c67d3f655cec21bc
jooooonis <xowns3213@naver.com>
2023-03-03 15:35:31 +0900

    feat : availability 날짜순으로 정렬

--- src/apis/event/eventApi.query.ts
+++ src/apis/event/eventApi.query.ts
@@ -4,7 +4,18 @@ import { useQuery } from 'react-query';
 import eventApi from './eventApi';
 
 export const useGetEventQuery = (uuid: string) => {
-  return useQuery<Event, AxiosError>(['event', uuid], () =>
-    eventApi.getEventById(uuid),
+  return useQuery<Event, AxiosError>(
+    ['event', uuid],
+    () => eventApi.getEventById(uuid),
+    {
+      select: (data) => {
+        const sortedDates = Object.keys(data.availability).sort();
+        const sortedObj = sortedDates.reduce((obj, key) => {
+          obj[key] = data.availability[key];
+          return obj;
+        }, {});
+        return { ...data, availability: sortedObj };
+      },
+    },
   );
 };
