163ae5d7217d31964c32c74a74ea43988c58afcc
jooooonis <xowns3213@naver.com>
2023-03-24 14:54:17 +0900

    chore : home 레이아웃 정리

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,7 +6,7 @@ import { useRouter } from 'next/router';
 function Home() {
   const router = useRouter();
   return (
-    <Layout>
+    <Layout className="h-screen">
       <Navigate />
       <div className="w-full flex flex-col items-center justify-center h-full">
         <div className="w-full flex flex-col items-center justify-center mb-4">
@@ -18,7 +18,7 @@ function Home() {
             모임을 만들고, 모임에 참여하고, 모임의 일정을 관리해보세요.
           </div>
         </div>
-        <div className="w-full space-y-2 flex flex-col items-center justify-center">
+        <div className="w-full mt-4 space-y-2 flex flex-col items-center justify-center">
           <Button onClick={() => router.push('/event/create/create01')}>
             모임 만들러 가기
           </Button>
