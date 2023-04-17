05ada0e805e6d63baeeca67febc79276459009c9
jooooonis <xowns3213@naver.com>
2023-04-17 16:37:49 +0900

    chore : button size

--- src/components/common/Button/FloatButton.tsx
+++ src/components/common/Button/FloatButton.tsx
@@ -24,17 +24,17 @@ export default function FloatButton({
       <div>
         <button
           className={`${
-            isFlipped && '-translate-y-[4.5rem]'
-          } fixed right-5 bottom-20 transition-all text-white text-12 mobile:right-[calc(50vw-175px)] w-16 aspect-square rounded-full bg-primary-green-1 ${className}`}
+            isFlipped && '-translate-y-[4rem]'
+          } fixed right-5 bottom-20 text-center transition-all text-white text-11 mobile:right-[calc(50vw-175px)] w-14 aspect-square rounded-full bg-primary-green-1 ${className}`}
           onClick={onClick}
           {...rest}
         >
-          일정 <br /> 추가
+          일정 <br /> 등록
         </button>
         <button
           className={`${
-            isFlipped && '-translate-y-[9rem]'
-          } fixed right-5 bottom-20 transition-all text-white text-12 mobile:right-[calc(50vw-175px)] w-16 aspect-square rounded-full bg-primary-green-1 ${className}`}
+            isFlipped && '-translate-y-[8rem]'
+          } fixed right-5 bottom-20 transition-all text-center text-white text-11 mobile:right-[calc(50vw-175px)] w-14 aspect-square rounded-full bg-primary-green-1 ${className}`}
           onClick={() => router.push('/event/create/create01')}
           {...rest}
         >
@@ -43,7 +43,7 @@ export default function FloatButton({
         <button
           className={`${
             isFlipped && 'bg-red-400'
-          } fixed transition-colors ease-linear right-5 bottom-20 mobile:right-[calc(50vw-175px)] w-16 aspect-square rounded-full bg-primary-green-1 ${className}`}
+          } fixed transition-colors ease-linear right-5 bottom-20 mobile:right-[calc(50vw-175px)] w-14 aspect-square rounded-full bg-primary-green-1 ${className}`}
           onClick={() => setIsFlipped(!isFlipped)}
           {...rest}
         >
